0d392f64c1041af0e2b5827ef26bac5cc119d12c
Bartosz Rozmus <bartekgit@gmail.com>
2026-09-11 09:44:22 +0200

    Zmienne

--- cwiczenia01/src/Main.java
+++ cwiczenia01/src/Main.java
@@ -11,4 +11,22 @@ System.out.println("Hello world"); // Wypisz i zrób enter
     System.out.println("\"cudzysłów\"");
     System.out.println("'\'apostrof\'");
     System.out.println("backflash: \\");
+
+    int liczbaCalkowita = 5;
+    double liczbaRzeczywista;
+
+    liczbaRzeczywista = (double) liczbaCalkowita/2;
+
+
+    System.out.println("Wynik dzielenia liczby 5 przez 2 to " + liczbaRzeczywista);
+
+    char znak = 'a';
+    System.out.println("Litera a ma wartość " + (znak + 0));
+
+    String napis = "napis";
+    System.out.println("Napis wielkimi literami: " + napis.toUpperCase());
+
+    System.out.println("Wynik 1+ 2 + 3 = " + 1 + 2 + 3);
+    System.out.println("Wynik 1 + 2 + 3 = " + (1 + 2 + 3));
+    System.out.println(1 + 2 + 3 + "to wynik 1 + 2 + 3");
 }
