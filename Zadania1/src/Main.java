0aeaf7d0a14a981ed0a6a7520c9668e85366b937
Bartosz Rozmus <3aBartek@gmail.com>
2026-09-16 14:07:31 +0200

    Dodanie projektu Zadania1

--- Zadania1/src/Main.java
+++ Zadania1/src/Main.java
@@ -1,13 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+//    System.out.println("Ania");
+//    System.out.println("Bartek");
+//    System.out.println("Kasia");
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+    String imie = "Jan";
+    int rokUrodzenia = 1995;
+    double liczba = 0.66;
+
+    System.out.println(imie);
+    System.out.println(rokUrodzenia);
+    System.out.println(liczba);
+
+
+
 }
