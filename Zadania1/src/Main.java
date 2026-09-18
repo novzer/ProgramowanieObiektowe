21a9d1730c5dc588bcbd170cdc1640c97cba6908
Bartosz Rozmus <3aklasa@gmail.com>
2026-09-18 09:42:29 +0200

    Zadania01

--- Zadania1/src/Main.java
+++ Zadania1/src/Main.java
@@ -1,16 +1,61 @@
-void main() {
+
+//    Zad.1
 //    System.out.println("Ania");
 //    System.out.println("Bartek");
 //    System.out.println("Kasia");
+//    Zad.2
+//    String imie = "Bartek";
+//    int rokUrodzenia = 2009;
+//    double liczba = 0.66;
+//    Zad.3
+//    System.out.println("Mam na imię " + imie + " mam " + (2026 - rokUrodzenia) + " lat" + " i będę pisać maturę za " + liczba + " roku");
+//    Zad.4
+//import java.util.Scanner;
+//
+//class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        double stopnie;
+//
+//        System.out.print("Podaj temperature na dworze w stopniach Celsjusza: ");
+//        stopnie = scanner.nextDouble();
+//
+//        double fahrenheit = 1.8 * stopnie + 32.0;
+//
+//        System.out.println("Temperatura w skali Fahrenheita: " + fahrenheit + " F");
+//    }
+//}
+//Zad.5
+//import java.util.Scanner;
+//
+//class ObwodTrojkata {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Podaj długość pierwszego boku (a): ");
+//        int a = scanner.nextInt();
+//
+//        System.out.print("Podaj długość drugiego boku (b): ");
+//        int b = scanner.nextInt();
+//
+//        System.out.print("Podaj długość trzeciego boku (c): ");
+//        int c = scanner.nextInt();
+//
+//        int obwod = a + b + c;
+//
+//        System.out.println("Obwód trójkąta wynosi: " + obwod);
+//
+//        scanner.close();
+//    }
+//}
 
-    String imie = "Jan";
-    int rokUrodzenia = 1995;
-    double liczba = 0.66;
-
-    System.out.println(imie);
-    System.out.println(rokUrodzenia);
-    System.out.println(liczba);
 
 
 
-}
+
+
+
+
+
+
