fe83f5f669d283ff97e24e8fcd57dd49bece3445
Bartek Rozmus <3aklasa@gmail.com>
2026-09-23 14:05:40 +0200

    Zadania02

--- Zadania02/src/Main.java
+++ Zadania02/src/Main.java
@@ -17,4 +17,54 @@
 //        scanner.close();
 //    }
 //}
-
+//Zad.2
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Podaj pierwszą liczbę: ");
+//        int liczba1 = scanner.nextInt();
+//
+//        System.out.print("Podaj drugą liczbę: ");
+//        int liczba2 = scanner.nextInt();
+//
+//        System.out.print("Podaj trzecią liczbę: ");
+//        int liczba3 = scanner.nextInt();
+//
+//        if (liczba1 > 0 && liczba2 > 0 && liczba3 > 0 &&
+//                liczba1 + liczba2 > liczba3 &&
+//                liczba1 + liczba3 > liczba2 &&
+//                liczba2 + liczba3 > liczba1)  {
+//            System.out.println("Z tych liczb może zostać zbudowany trójkąt");
+//        } else {
+//            System.out.println("Z tych liczb nie może zostać zbudowany trójkąt");
+//        }
+//
+//        scanner.close();
+//    }
+//}
+//Zad.3
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Podaj pierwszą liczbę: ");
+//        int a = scanner.nextInt();
+//
+//        System.out.print("Podaj drugą liczbę: ");
+//        int b = scanner.nextInt();
+//
+//
+//        if (a > b)  {
+//            System.out.println("Największą z liczb jest pierwsza");
+//        } else {
+//            System.out.println("Największą z liczb jest druga");
+//        }
+//
+//        scanner.close();
+//    }
+//}
