5a6718a5bc6fd723e6b653111e7e58409b8eabf5
Bartek Rozmus <3aklasa@gmail.com>
2026-09-23 13:45:59 +0200

    Zadania02

---
+++ Zadania02/src/Main.java
@@ -0,0 +1,19 @@
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Podaj liczbę: ");
+//        int liczba = scanner.nextInt();
+//
+//        if (liczba % 3 == 0) {
+//            System.out.println("Liczba jest podzielna przez 3.");
+//        } else {
+//            System.out.println("Liczba nie jest podzielna przez 3.");
+//        }
+//
+//        scanner.close();
+//    }
+//}
+
